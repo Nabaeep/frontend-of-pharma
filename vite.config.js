c79905135f67545bd4e952b598ff3d8d05d07a88
Nabaeep <nabadeepdutta8@gmail.com>
2025-10-12 14:37:39 +0530

    Fix Vercel routing for React Router

--- vite.config.js
+++ vite.config.js
@@ -4,6 +4,17 @@ import react from '@vitejs/plugin-react'
 
 // https://vite.dev/config/
 export default defineConfig({
-  plugins: [tailwindcss(),react()],
-  
+  plugins: [tailwindcss(), react()],
+  build: {
+    outDir: 'dist',
+  },
+  server: {
+    port: 5173,
+    open: true,
+  },
+  // ✅ Enable React Router history fallback
+  preview: {
+    port: 4173,
+    open: true,
+  },
 })
